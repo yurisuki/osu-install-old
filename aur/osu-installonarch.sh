29b04da01d91ea0aaf6476239b426ddfecc3af9e
yurisuki <yurisuki@waifu.club>
2019-04-14 11:49:51 +0200

    new version woah

--- aur/osu-installonarch.sh
+++ aur/osu-installonarch.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 # This script will install osu! using ThePooN's method and tweaks.
 # Depends on: wine, dialog..
 
@@ -23,16 +23,16 @@ welcome() { # This will greet you :)
 }
 
 archpreparing() { # This one will prepare all scripts for installation (For arch-based distro)
-	dialog --title "$tits" --infobox "Preparing some stuff before starting the installation." 7 50
+	dialog --title "$tits" --infobox "I've detected that you use Arch-based distro. \n\n\nPreparing some stuff before starting the installation." 7 50
 	sleep 5
 	dialog --title "$tits" --msgbox "You are currently entering sudo mode.\n\nYou will be prompted for password, so enter it in.\n\n\n$continue" 11 50
 	sudo pacman-key --keyserver hkps://hkps.pool.sks-keyservers.net -r C0E7D0CDB72FBE95
 	sudo pacman-key --keyserver hkps://hkps.pool.sks-keyservers.net --lsign-key C0E7D0CDB72FBE95
-	cat /etc/pacman.conf | grep thepoon || sudo bash -c 'echo "
+	cat /etc/pacman.conf | grep thepoon || echo "
 [thepoon]
 Server = https://archrepo.thepoon.fr
-Server = https://mirrors.celianvdb.fr/archlinux/thepoon" >> /etc/pacman.conf'
-	sudo pacman --noconfirm -Syy wine-osu
+Server = https://mirrors.celianvdb.fr/archlinux/thepoon" | sudo tee -a /etc/pacman.conf
+	sudo pacman --noconfirm -Syy wine-osu winetricks wine-mono lib32-gnutls lib32-alsa-plugins lib32-libpulse lib32-openal p7zip
 }
 
 setupwine() { # This one will make wineprefix and will install osu!.
@@ -62,7 +62,7 @@ IN THE TERMINAL TO MAKE THE SCRIPT CONTINUE!\n\n\n$continue" 13 45 || fuck "You'
 
 fixlatency() { # This one will set up some things after setupwine has succeeded
 	echo "$USER" > /tmp/osuuser
-	sudo bash -c 'echo -e "$(cat /tmp/osuuser) - nice -20\n$(cat /tmp/osuuser) - rtprio 99" > /etc/security/limits.conf'
+	echo -e "$(cat /tmp/osuuser) - nice -20\n$(cat /tmp/osuuser) - rtprio 99" | sudo tee /etc/security/limits.conf
 
 	sudo mkdir -p /etc/pulse/daemon.conf.d/
 	echo "high-priority = yes
@@ -83,7 +83,7 @@ default-fragment-size-msec = 2 # You can set this to 1, but that will break OBS 
 afterwine() { # This one will make osu! scripts
 	ln -sv ~/.wine_osu/drive_c/users/$USER/Local\ Settings/Application\ Data/osu\!/ ~/osudir
 
-	echo '#!/bin/bash
+	echo '#!/usr/bin/env bash
 # This script will fix the PulseAudio crack.
 
 ok() {
@@ -95,9 +95,9 @@ echo -e "Sound has been fixed!\nReboot your computer for these changes to take e
 
 cat /etc/pulse/default.pa | grep tsched=0 || exit && echo -e "The sound cracking is already fixed." && sudo rm /bin/fixcrack
 
-ok && sudo rm /bin/fixcrack' | sudo tee -a /bin/fixcrack
+ok && sudo rm /bin/fixcrack' | sudo tee /bin/fixcrack
 
-	echo '#!/bin/sh
+	echo '#!/usr/bin/env bash
 export WINEPREFIX="$HOME/.wine_osu"
 export STAGING_AUDIO_DURATION=10000 # This one is the most stable value for most people, you can change this down to 5000 (that is the lowest stable value osu! can handle.)
 
@@ -108,10 +108,10 @@ export PATH="/opt/wine-osu/bin:$PATH"
 cd ~/osudir
 wine osu!.exe "$@"' | sudo tee /bin/osu
 
-	echo '#!/bin/sh
+	echo '#!/usr/bin/env bash
 export WINEPREFIX="$HOME/.wine_osu"
 
-wineserver -k' | sudo tee -a /bin/osukill
+wineserver -k' | sudo tee /bin/osukill
 	sudo chmod +x /bin/osukill /bin/osu /bin/fixcrack
 	curl --output ~/.local/share/icons/osu\!.png https://upload.wikimedia.org/wikipedia/commons/thumb/e/e3/Osulogo.png/220px-Osulogo.png
 	echo '[Desktop Entry]
@@ -121,7 +121,7 @@ Icon=osu!.png
 Exec=osu %u
 StartupWMClass=osu!.exe
 Categories=Game;
-MimeType=x-scheme-handler/discord-367827983903490050;x-scheme-handler/osu;' > ~/.local/share/applications/osu\!.desktop
+MimeType=x-scheme-handler/discord-367827983903490050;x-scheme-handler/osu;' | tee ~/.local/share/applications/osu\!.desktop
 }
 
 selection() { # Make dialog select menu for additional things
@@ -166,8 +166,8 @@ dialog --help || fuck "Dialog is not installed."
 # Welcome message
 welcome || fuck "Interupted by user."
 
-# AUR! Install do Arch things
-archpreparing
+# After welcome scripts
+afterwelcome
 
 # Setup wine after all dependencies were installed
 setupwine
